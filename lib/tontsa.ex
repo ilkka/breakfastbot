c2dcaccf68fb78dbf48ea710e5d0aeaf7c1d6587
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2019-10-25 14:13:05 +0200

    Initial commit

---
+++ lib/tontsa.ex
@@ -0,0 +1,18 @@
+defmodule Tontsa do
+  @moduledoc """
+  Documentation for Tontsa.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tontsa.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tontsa_test.exs
@@ -0,0 +1,8 @@
+defmodule TontsaTest do
+  use ExUnit.Case
+  doctest Tontsa
+
+  test "greets the world" do
+    assert Tontsa.hello() == :world
+  end
+end
